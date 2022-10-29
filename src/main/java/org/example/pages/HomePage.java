3354dca8e091188fdcdb01a922933dea93d9d869
Ivan <vintonyaki1990@gmail.com>
2022-10-29 20:29:30 +0300

    first commit

--- src/main/java/org/example/pages/HomePage.java
+++ src/main/java/org/example/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage {
 
-    private static final String CUCUMBER_URL = "https://cucumber.io";
+    private static final String CUCUMBER_URL = "https://cucumber.io/";
     private static final String MENU_SECTION = "//a[contains(@class, 'nav-link')]";
     private static final String TEXT_PATTERN = "%s[contains(text(), '%s')]";
     private static final String CHILD_MENU_ITEM = "//*[contains(@class, 'dropdown')]//*[contains(@class, 'item')]";
